b159133b76d85800392a550dcb7861d1c191fb53
abser <abserari@gmail.com>
2020-02-12 22:31:16 +0800

    update ws upgrader

--- ws/server.go
+++ ws/server.go
@@ -7,10 +7,14 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var upGrader = websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
 
 func ping(c *gin.Context) {
-	ws, err := upGrader.Upgrade(c.Writer, c.Request, nil)
+	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
 		return
 	}
