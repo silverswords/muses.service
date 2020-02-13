b09161347fdc8207b2d8a78e5bbe3243e6578170
flutterWang <wangbofeng980404@gmail.com>
2020-02-13 20:13:37 +0800

    update apis

--- apis/router.go
+++ apis/router.go
@@ -7,14 +7,42 @@ import (
 func InitRouter() *gin.Engine {
 	r := gin.Default()
 
+	apiGrp(r)
+
+	return r
 }
 
 func apiGrp(r *gin.Engine) {
 	apiGrp := r.Group("/api/v1")
 
+	// users
 	userGrp := apiGrp.Group("/user")
 	{
-		userGrp.POST("/default")
+		userGrp.POST("/default") // 游客模式
 		userGrp.POST("/login")
+		userGrp.POST("/joinRoom")
+		userGrp.POST("/leaveRoom")
+		userGrp.POST("/sendMsg")
+
+		userGrp.POST("/register")
+		userGrp.POST("/removeUser")
+	}
+
+	// room
+	roomGrp := apiGrp.Group("/room")
+	{
+		roomGrp.POST("/getRoomList")
+		roomGrp.POST("/getRoomInfo")
+
+		roomGrp.POST("/createRoom")
+		roomGrp.POST("/removeRoom")
+	}
+
+	// file
+	fileGrp := apiGrp.Group("/file")
+	{
+		fileGrp.POST("/listFile")
+		fileGrp.POST("/uploadFile")
+		fileGrp.POST("/removeFile")
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module muses.service
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.5.0 // indirect
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/gorilla/websocket v1.4.1
+	gopkg.in/olahol/melody.v1 v1.0.0-20170518105555-d52139073376 // indirect
+)
