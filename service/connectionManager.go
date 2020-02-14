bc838426c231621a063a8b570948e32a92f7b440
abser <abserari@gmail.com>
2020-02-15 00:58:14 +0800

    add client behavior

--- service/connectionManager.go
+++ service/connectionManager.go
@@ -10,7 +10,32 @@ import (
 	"github.com/go-redis/redis/v7"
 )
 
-type Conn struct {
+type Conn interface{
+		// ID returns session id
+		ID() string
+		Close() error
+		URL() url.URL
+		LocalAddr() net.Addr
+		RemoteAddr() net.Addr
+		RemoteHeader() http.Header
+	
+		// Context of this connection. You can save one context for one
+		// connection, and share it between all handlers. The handlers
+		// is called in one goroutine, so no need to lock context if it
+		// only be accessed in one connection.
+		Context() interface{}
+		SetContext(v interface{})
+		Namespace() string
+		Emit(msg string, v ...interface{})
+	
+		// Broadcast server side apis
+		Join(room string)
+		Leave(room string)
+		LeaveAll()
+		Rooms() []string
+}
+
+type conn struct {
 	userid uint64
 	wsC *websocket.Conn
 	redisC *redis.Client
@@ -27,7 +52,7 @@ func NewConn(wsc *websocket.Conn,redisOptions *redis.Options) *Conn {
 	return c
 }
 
-func (c *Conn) isUserOnline(username string) bool {
+func (c *conn) isUserOnline(username string) bool {
 	userkey := "online." + username
 	set, err := c.redisC.SetNX(userkey, username, 10*time.Second).Result()
 	if err != nil {
