0d96fab2fd37583f1879603c99e5670091bd016c
abser <abserari@gmail.com>
2020-02-14 19:59:47 +0800

    redis sample

--- go.mod
+++ go.mod
@@ -6,6 +6,9 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gbrlsnchs/jwt/v3 v3.0.0-rc.1
 	github.com/gin-gonic/gin v1.5.0
+	github.com/go-redis/redis v6.15.7+incompatible
 	github.com/gorilla/websocket v1.4.1
+	github.com/onsi/ginkgo v1.12.0 // indirect
+	github.com/onsi/gomega v1.9.0 // indirect
 	github.com/satori/go.uuid v1.2.0
 )
---
+++ service/service_a.go
@@ -0,0 +1,81 @@
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+package main
+
+import (
+	"flag"
+
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-redis/redis"
+
+	"strconv"
+
+	"github.com/gorilla/websocket"
+)
+
+var addr = flag.String("addr", "localhost:8082", "http service address")
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+var client = redis.NewClient(&redis.Options{
+	Addr:     "localhost:6379",
+	Password: "", // no password set
+	DB:       0,  // use default DB
+})
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("good")
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, _, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		// log.Printf("recv: %s", message)
+
+		key := "counter"
+		myval, _ := client.Get(key).Result()
+		intv, _ := strconv.Atoi(string(myval))
+		fmt.Println(intv)
+		intstr := strconv.Itoa(intv)
+
+		err = c.WriteMessage(mt, []byte(intstr))
+
+		log.Printf("send: %s", intstr)
+
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+func main() {
+
+	// // Read the value back out of the store.
+
+	flag.Parse()
+	log.SetFlags(0)
+	fmt.Println("good")
+
+	http.HandleFunc("/echo", echo)
+	// http.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
