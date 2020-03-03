ef23360051a010adefdc798a02cf60094afdff43
abser <abserari@gmail.com>
2020-03-03 13:11:41 +0800

    add comment to eventbus

--- service/eventbus/eventbus.go
+++ service/eventbus/eventbus.go
@@ -7,8 +7,9 @@ import (
 	"sync"
 )
 
-//https://github.com/grpc/grpc-go/blob/master/stream.go#L122:2
-//stream read write
+//if need stream read write, read about https://github.com/grpc/grpc-go/blob/master/stream.go#L122:2
+
+// Endpoint means a middleware which does nothing more but send and receive Event.
 type Endpoint interface {
 	Descriptor() string
 	Receive() (Event, error)
@@ -16,40 +17,51 @@ type Endpoint interface {
 	Probe() bool
 }
 
+// EventBus manager the register receiver and send Event to them.
 type EventBus interface {
 	Attach(topic string) Endpoint
 	Send(topic string, e Event)
 }
 
+// Event is messages' type.
 type Event interface {
 	Type() string
 	Payload() []byte
 }
 
+// Pong realized Event.
 type Pong struct {
 	pID     string
 	payload []byte
 }
 
+// NewMessage new a pong struct.
 func NewMessage(pid string, payload []byte) Pong { return Pong{pID: pid, payload: payload} }
-func (p Pong) Type() string                      { return p.pID }
-func (p Pong) Payload() []byte                   { return p.payload }
 
-type Redbus struct {
+// Type return Event type.
+func (p Pong) Type() string { return p.pID }
+
+// Payload return data of Event.
+func (p Pong) Payload() []byte { return p.payload }
+
+// Bus relized Eventbus
+type Bus struct {
 	epcount int
 	eps     map[string]map[int]chan Event
 	rm      sync.RWMutex
 }
 
-func New() *Redbus {
-	return &Redbus{
+// New return Bus struct pointer.
+func New() *Bus {
+	return &Bus{
 		epcount: 0,
 		eps:     make(map[string]map[int]chan Event),
 		rm:      sync.RWMutex{},
 	}
 }
 
-func (eb *Redbus) Attach(topic string) Endpoint {
+// Attach return Endpoint to receive events from the specified topic.
+func (eb *Bus) Attach(topic string) Endpoint {
 	eb.rm.Lock()
 	ep := eb.Newendpoint(topic + " " + strconv.Itoa(eb.epcount))
 	if _, found := eb.eps[topic]; !found {
@@ -61,7 +73,8 @@ func (eb *Redbus) Attach(topic string) Endpoint {
 	return ep
 }
 
-func (eb *Redbus) Send(topic string, data Event) {
+// Send send event from bus to target topic's receiver.
+func (eb *Bus) Send(topic string, data Event) {
 	eb.rm.RLock()
 	if eps, found := eb.eps[topic]; found {
 		go func(data Event, eps map[int]chan Event) {
@@ -74,22 +87,24 @@ func (eb *Redbus) Send(topic string, data Event) {
 }
 
 type endpoint struct {
-	eb         Redbus
+	eb         Bus
 	descriptor string
 	Ch         chan Event
 }
 
-func (eb *Redbus) Newendpoint(descriptor string) *endpoint {
+// Newendpoint return endpoint struct pointer.
+func (eb *Bus) Newendpoint(descriptor string) *endpoint {
 	return &endpoint{eb: *eb, descriptor: descriptor, Ch: make(chan Event, 100)}
 }
 
+// Send endpoint could also send event to other endpoint by bus.
 func (ep *endpoint) Send(topic string, e Event) (err error) {
 	//add time out control
 	ep.eb.Send(topic, e)
 	return nil
 }
 
-// a block method until receive a event from the eventbus
+// Receice is a block method until receive a event from the eventbus.
 func (ep *endpoint) Receive() (data Event, err error) {
 	data, haddata := <-ep.Ch
 	if !haddata {
@@ -98,6 +113,7 @@ func (ep *endpoint) Receive() (data Event, err error) {
 	return data, nil
 }
 
+// Detach detach current endpoint from bus.
 func (ep *endpoint) Detach() { //	close channel on EventBus
 	desc := strings.Split(ep.descriptor, " ")
 	epcount, _ := strconv.Atoi(desc[1])
@@ -105,6 +121,7 @@ func (ep *endpoint) Detach() { //	close channel on EventBus
 	close(ep.Ch)
 }
 
+// Probe detects the bus if have this endpoint. In case to know if avialable to send or receive Event.
 func (ep *endpoint) Probe() bool {
 	desc := strings.Split(ep.descriptor, " ")
 	epcount, _ := strconv.Atoi(desc[1])
@@ -114,4 +131,5 @@ func (ep *endpoint) Probe() bool {
 	return true
 }
 
+// Descriptor return the info of the endpoint.
 func (ep *endpoint) Descriptor() string { return ep.descriptor }
